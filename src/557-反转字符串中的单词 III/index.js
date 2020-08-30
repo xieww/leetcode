df88be82ff532853a054ec8c2105a63cd7a7d34d
xieww <2801416900@qq.com>
2020-08-30 22:53:15 +0800

    feat(LeetCode): 反转字符串中的单词 III

--- src/557-反转字符串中的单词 III/index.js
+++ src/557-反转字符串中的单词 III/index.js
@@ -2,14 +2,28 @@
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function (s) {
-  const arr = s.split(" ");
+// 方法1
+// var reverseWords = function (s) {
+//   const arr = s.split(" ");
 
-  for (let i = 0; i < arr.length; i++) {
-    const current = arr[i].split("").reverse().join("");
-    arr[i] = current;
-  }
-  return arr.join(" ");
+//   for (let i = 0; i < arr.length; i++) {
+//     const current = arr[i].split("").reverse().join("");
+//     arr[i] = current;
+//   }
+//   return arr.join(" ");
+// };
+
+// 方法2
+// var reverseWords = function (s) {
+//   return s
+//     .split(" ")
+//     .map((v) => [...v].reverse().join("")) // Array.from(v).reverse().join("")
+//     .join(" ");
+// };
+
+// 方法3
+var reverseWords = function (s) {
+  return [...s].reverse().join("").split(" ").reverse().join(" ");
 };
 
 console.log(
