be5b7f94515d680e7b483a60a3524150fba0d242
Chasen.xie <chasen.xie@trustasia.com>
2020-08-31 09:56:38 +0800

    feat(LeetCode): 字符串中的第一个唯一字符

---
+++ src/387-字符串中第一个唯一字符/index.js
@@ -0,0 +1,54 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+// 方法1（可能会超时）
+// var firstUniqChar = function (s) {
+//   let result = -1;
+//   for (let i = 0; i < s.length; i++) {
+//     const regex = new RegExp(s[i], "g");
+//     if (s.match(regex) !== null && s.match(regex).length === 1) {
+//       result = i;
+//       break;
+//     }
+//   }
+//   return result;
+// };
+
+// 方法2
+// var firstUniqChar = function (s) {
+//   for (let i = 0; i < s.length; i++) {
+//     const regex = new RegExp(s[i], "g");
+//     const str = s.replace(regex, "");
+//     if (s.length - str.length === 1) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// };
+
+// 方法3
+var firstUniqChar = function (s) {
+  for (let i = 0; i < s.length; i++) {
+    const arr = s.split(s[i]);
+    if (arr && arr.length === 2) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+// 方法4
+// var firstUniqChar = function (s) {
+//   for (let i = 0; i < s.length; i++) {
+//     if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// };
+
+console.log("should be 0", firstUniqChar("leetcode"));
+console.log("should be 2", firstUniqChar("loveleetcode"));
+console.log("should be -1", firstUniqChar("leetcodeleetcode"));
