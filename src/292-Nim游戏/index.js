6ff4fcf223178f801b85f71c0cfb7a3da2751a48
Chasen.xie <chasen.xie@trustasia.com>
2020-09-08 09:33:59 +0800

    feat(LeetCode): Nim游戏

--- src/292-Nim游戏/index.js
+++ src/292-Nim游戏/index.js
@@ -2,14 +2,14 @@
  * @param {number} n
  * @return {boolean}
  */
-// 方法1 时间复杂度O(n),空间复杂度O(n)
+// 方法1 时间复杂度O(1),空间复杂度O(1)
 // var canWinNim = function (n) {
 //   return n % 4 !== 0;
 // };
 
 const memo = { 0: false, 1: true, 2: true, 3: true, 4: false };
 
-// 方法2 时间复杂度O(1),空间复杂度O(1)
+// 方法2 时间复杂度O(n),空间复杂度O(n)
 var canWinNim = function (n) {
   if (memo[n] === undefined) {
     memo[n] = !canWinNim(n - 3) || !canWinNim(n - 2) || !canWinNim(n - 1);
