3f968970d5da176131190d1d6ae4b956a1eabc11
xieww <2801416900@qq.com>
2020-09-20 10:09:50 +0800

    feat(LeetCode): 左叶子之和

--- src/404-左叶子之和/index.js
+++ src/404-左叶子之和/index.js
@@ -11,20 +11,20 @@
  */
 var sumOfLeftLeaves = function (root) {
   let sum = 0;
-  var leafTree = function (node) {
+  var dfs = function (node) {
     if (!node) {
       return 0;
     }
     if (!node.left && !node.right) {
       return node.val;
     }
-    const leftLeave = leafTree(node.left);
-    if (leftLeave) {
-      sum += leftLeave;
+    const num = dfs(node.left);
+    if (num) {
+      sum += num;
     }
-    leafTree(node.right);
+    dfs(node.right);
   };
-  leafTree(root);
+  dfs(root);
   return sum;
 };
 
@@ -38,34 +38,27 @@ var sumOfLeftLeaves = function (root) {
   return recurse(root);
 };
 
-// var sumOfLeftLeaves = function (root) {
-//   if (!root || (!root.left && !root.right)) {
-//     return 0;
-//   }
+// BFS
+var sumOfLeftLeaves = function (root) {
+  if (!root || (!root.left && !root.right)) {
+    return 0;
+  }
+  let sum = 0;
+  let queue = [root];
+  while (queue.length) {
+    const node = queue.pop(); // queue.shift()
+    if (node.left) {
+      const nodeLeft = node.left;
+      if (!nodeLeft.left && !nodeLeft.right) {
+        sum += nodeLeft.val;
+      } else {
+        queue.unshift(nodeLeft);
+      }
+    }
+    if (node.right) {
+      queue.unshift(node.right);
+    }
+  }
 
-//   let sum = 0;
-
-//   let queue = [];
-
-//   queue.unshift(root);
-
-//   while (queue.length) {
-//     const node = queue.pop();
-
-//     if (node.left) {
-//       const nodeLeft = node.left;
-
-//       if (!nodeLeft.left && !nodeLeft.right) {
-//         sum += nodeLeft.val;
-//       } else {
-//         queue.unshift(nodeLeft);
-//       }
-//     }
-
-//     if (node.right) {
-//       queue.unshift(node.right);
-//     }
-//   }
-
-//   return sum;
-// };
+  return sum;
+};
