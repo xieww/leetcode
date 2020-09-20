ab38b1a05c439588753888da765fbfffb978701a
xieww <2801416900@qq.com>
2020-09-20 16:52:35 +0800

    feat(LeetCode): 用栈实现队列

--- src/232-用栈实现队列/index.js
+++ src/232-用栈实现队列/index.js
@@ -9,58 +9,56 @@
 /**
  * Initialize your data structure here.
  */
-var MyQueue = function () {
-  this.stack1 = [];
-  this.stack2 = [];
-};
-
-/**
- * Push element x to the back of queue.
- * @param {number} x
- * @return {void}
- */
-MyQueue.prototype.push = function (x) {
-  this.stack1.push(x);
-};
-
-/**
- * Removes the element from in front of queue and returns that element.
- * @return {number}
- */
-MyQueue.prototype.pop = function () {
-  while (this.stack1.length !== 0) {
-    this.stack2.push(this.stack1.pop());
+class MyQueue {
+  constructor() {
+    this.stack1 = [];
+    this.stack2 = [];
   }
-  var pop = this.stack2.pop();
-  while (this.stack2.length !== 0) {
-    this.stack1.push(this.stack2.pop());
+  /**
+   * Push element x to the back of queue.
+   * @param {number} x
+   * @return {void}
+   */
+  push(x) {
+    this.stack1.push(x);
   }
-  return pop;
-};
-
-/**
- * Get the front element.
- * @return {number}
- */
-MyQueue.prototype.peek = function () {
-  while (this.stack1.length !== 0) {
-    this.stack2.push(this.stack1.pop());
+  /**
+   * Removes the element from in front of queue and returns that element.
+   * @return {number}
+   */
+  pop() {
+    while (this.stack1.length !== 0) {
+      this.stack2.push(this.stack1.pop());
+    }
+    var pop = this.stack2.pop();
+    while (this.stack2.length !== 0) {
+      this.stack1.push(this.stack2.pop());
+    }
+    return pop;
   }
-  var pop = this.stack2.pop();
-  this.stack2.push(pop);
-  while (this.stack2.length !== 0) {
-    this.stack1.push(this.stack2.pop());
+  /**
+   * Get the front element.
+   * @return {number}
+   */
+  peek() {
+    while (this.stack1.length !== 0) {
+      this.stack2.push(this.stack1.pop());
+    }
+    var pop = this.stack2.pop();
+    this.stack2.push(pop);
+    while (this.stack2.length !== 0) {
+      this.stack1.push(this.stack2.pop());
+    }
+    return pop;
   }
-  return pop;
-};
-
-/**
- * Returns whether the queue is empty.
- * @return {boolean}
- */
-MyQueue.prototype.empty = function () {
-  return this.stack1.length === 0;
-};
+  /**
+   * Returns whether the queue is empty.
+   * @return {boolean}
+   */
+  empty() {
+    return this.stack1.length === 0;
+  }
+}
 
 const queue = new MyQueue();
 
