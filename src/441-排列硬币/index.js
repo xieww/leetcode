c45d48198de3eee37c5ddd6799204ec61b90104d
xieww <2801416900@qq.com>
2020-09-26 16:44:29 +0800

    feat(LeetCode): 排列硬币

--- src/441-排列硬币/index.js
+++ src/441-排列硬币/index.js
@@ -2,6 +2,7 @@
  * @param {number} n
  * @return {number}
  */
+// 方法1
 var arrangeCoins = function (n) {
   let stairs = 0;
   while (stairs <= n) {
@@ -11,4 +12,19 @@ var arrangeCoins = function (n) {
   return stairs - 1;
 };
 
+// 方法2
+var arrangeCoins = function (n) {
+  return Math.floor(Math.sqrt(2 * n + 1 / 4) - 1 / 2);
+};
+
+// 方法3
+var arrangeCoins = function (n) {
+  let guess = Math.round(Math.sqrt(2 * n));
+  let used_coins = (guess * (guess + 1)) / 2;
+  while (used_coins <= n) {
+    guess++;
+    used_coins = guess * (guess + 1); // 2;
+  }
+  return guess > 0 ? guess - 1 : 0;
+};
 console.log("should be 2", arrangeCoins(5));
