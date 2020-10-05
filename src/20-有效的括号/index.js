5658f381ecb5b36f0da2e3ab7b9b567b41e990e5
xieww <2801416900@qq.com>
2020-10-05 09:37:10 +0800

    feat(LeetCode): 颜色分类

--- src/20-有效的括号/index.js
+++ src/20-有效的括号/index.js
@@ -38,6 +38,39 @@ var isValid = function (s) {
   return !arr.length;
 };
 
+var isValid = function (s) {
+  let map = {
+    "(": -1,
+    ")": 1,
+    "[": -2,
+    "]": 2,
+    "{": -3,
+    "}": 3,
+  };
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] < 0) {
+      stack.push(s[i]);
+    } else {
+      let last = stack.pop();
+      if (map[last] + map[s[i]] != 0) return false;
+    }
+  }
+  if (stack.length > 0) return false;
+  return true;
+};
+
+var isValid = function (s) {
+  while (s.length) {
+    var temp = s;
+    s = s.replace("()", "");
+    s = s.replace("[]", "");
+    s = s.replace("{}", "");
+    if (s === temp) return false;
+  }
+  return true;
+};
+
 console.log("() is true", isValid("()"));
 console.log("()[]{} is true", isValid("()[]{}"));
 console.log("(] is false", isValid("(]"));
