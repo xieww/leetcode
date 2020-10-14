94b87f2f94ec39456c6da6aa70b5b2ab32c3d74f
Chasen.xie <chasen.xie@trustasia.com>
2020-10-14 10:13:07 +0800

    feat(LeetCode): 查找常用字符

--- src/1002-查找常用字符/index.js
+++ src/1002-查找常用字符/index.js
@@ -2,6 +2,7 @@
  * @param {string[]} A
  * @return {string[]}
  */
+// 方法1
 // var commonChars = function (A) {
 //   let result = A[0].split("");
 //   for (let i = 1; i < A.length; i++) {
@@ -10,17 +11,18 @@
 //   return result;
 // };
 
-var findCommon = function (a, b) {
-  return a.filter((v) => {
-    let i = b.indexOf(v);
-    if (i !== -1) {
-      b.splice(i, 1);
-      return true;
-    }
-    return false;
-  });
-};
+// var findCommon = function (a, b) {
+//   return a.filter((v) => {
+//     let i = b.indexOf(v);
+//     if (i !== -1) {
+//       b.splice(i, 1);
+//       return true;
+//     }
+//     return false;
+//   });
+// };
 
+// 方法2
 var commonChars = function (A) {
   let result = [...A[0]];
   for (let i = 0; i < A.length; i++) {
@@ -32,6 +34,55 @@ var commonChars = function (A) {
   }
   return result;
 };
+
+// 方法3
+// var commonChars = function (A) {
+//   return A.reduce(findCommon, A[0].split(""));
+// };
+
+// const findCommon = (prev, curr) => {
+//   curr = curr.split("");
+//   return prev.filter((p) => {
+//     const i = curr.indexOf(p);
+//     if (i !== -1) {
+//       curr.splice(i, 1);
+//       return true;
+//     }
+//     return false;
+//   });
+// };
+
+// 方法4
+// var commonChars = function (A) {
+//   if (A.length == 0) return [];
+//   return A[0]
+//     .split("")
+//     .map((m) => {
+//       for (var i = 1; i < A.length; i++) {
+//         var index = A[i].indexOf(m);
+//         if (index == -1) {
+//           return null;
+//         } else {
+//           A[i] = A[i].replace(m, "");
+//         }
+//       }
+//       return m;
+//     })
+//     .filter((m) => m != null);
+// };
+
+// 方法5
+// var commonChars = function (A) {
+//   let result = [];
+//   const word = A[0];
+//   for (let s of word) {
+//     if (A.every((m) => m.includes(s))) {
+//       A = A.map((m) => m.replace(s, ""));
+//       result.push(s);
+//     }
+//   }
+//   return result;
+// };
 
 console.log(
   'should be ["e","l","l"]',
