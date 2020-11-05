3448713257c6d8dcf347e11369855c377325390b
Chasen.xie <chasen.xie@trustasia.com>
2020-11-05 09:55:47 +0800

    feat(LeetCode): 单词接龙

--- src/127-单词接龙/index.js
+++ src/127-单词接龙/index.js
@@ -38,44 +38,44 @@ var ladderLength = function (beginWord, endWord, wordList) {
 };
 
 // 双端BFS
-let ladderLength = function (beginWord, endWord, wordList) {
-  let wordListSet = new Set(wordList);
-  if (!wordListSet.has(endWord)) {
-    return 0;
-  }
-  let beginSet = new Set();
-  beginSet.add(beginWord);
-  let endSet = new Set();
-  endSet.add(endWord);
-  let level = 1;
-  // BFS
-  while (beginSet.size > 0) {
-    let next_beginSet = new Set();
-    for (let key of beginSet) {
-      for (let i = 0; i < key.length; i++) {
-        for (let j = 0; j < 26; j++) {
-          let s = String.fromCharCode(97 + j);
-          if (s != key[i]) {
-            let new_word = key.slice(0, i) + s + key.slice(i + 1);
-            if (endSet.has(new_word)) {
-              return level + 1;
-            }
-            if (wordListSet.has(new_word)) {
-              next_beginSet.add(new_word);
-              wordListSet.delete(new_word);
-            }
-          }
-        }
-      }
-    }
-    beginSet = next_beginSet;
-    level++;
-    if (beginSet.size > endSet.size) {
-      [beginSet, endSet] = [endSet, beginSet];
-    }
-  }
-  return 0;
-};
+// var ladderLength = function (beginWord, endWord, wordList) {
+//   let wordListSet = new Set(wordList);
+//   if (!wordListSet.has(endWord)) {
+//     return 0;
+//   }
+//   let beginSet = new Set();
+//   beginSet.add(beginWord);
+//   let endSet = new Set();
+//   endSet.add(endWord);
+//   let level = 1;
+//   // BFS
+//   while (beginSet.size > 0) {
+//     let next_beginSet = new Set();
+//     for (let key of beginSet) {
+//       for (let i = 0; i < key.length; i++) {
+//         for (let j = 0; j < 26; j++) {
+//           let s = String.fromCharCode(97 + j);
+//           if (s != key[i]) {
+//             let new_word = key.slice(0, i) + s + key.slice(i + 1);
+//             if (endSet.has(new_word)) {
+//               return level + 1;
+//             }
+//             if (wordListSet.has(new_word)) {
+//               next_beginSet.add(new_word);
+//               wordListSet.delete(new_word);
+//             }
+//           }
+//         }
+//       }
+//     }
+//     beginSet = next_beginSet;
+//     level++;
+//     if (beginSet.size > endSet.size) {
+//       [beginSet, endSet] = [endSet, beginSet];
+//     }
+//   }
+//   return 0;
+// };
 
 console.log(
   "should be ",
