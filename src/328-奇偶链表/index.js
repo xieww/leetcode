942aa50d5aa4c73825639b2d86c8c1ccfd507951
Chasen.xie <chasen.xie@trustasia.com>
2020-11-13 10:00:11 +0800

    feat(LeetCode): 奇偶链表

--- src/328-奇偶链表/index.js
+++ src/328-奇偶链表/index.js
@@ -11,13 +11,29 @@
  */
 var oddEvenList = function (head) {
   if (head !== null) {
+    let odd = head; // 奇数节点
+    let even = head.next; // 偶数节点
+    let evenHead = even;
+    while (even !== null && even.next !== null) {
+      odd.next = odd.next.next;// 奇数节点的后继指向其后继节点的后继
+      even.next = even.next.next; // 偶数节点的后继指向其后继节点的后继
+      odd = odd.next;
+      even = even.next;
+    }
+    odd.next = evenHead; // 将最终的奇数节点后继指向偶数节点
+  }
+  return head;
+};
+
+var oddEvenList = function (head) {
+  if (head !== null) {
     let odd = head;
     let even = head.next;
     let evenHead = even;
     while (even !== null && even.next !== null) {
-      odd.next = odd.next.next;
-      even.next = even.next.next;
+      odd.next = even.next; // 奇数节点的后继指向偶数节点的后继
       odd = odd.next;
+      even.next = odd.next; // 偶数节点的后继指向奇数节点的后继
       even = even.next;
     }
     odd.next = evenHead;
