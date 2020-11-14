ec27a70fa069d7628f4a9c0a940525a1d3c0c02c
xieww <2801416900@qq.com>
2020-11-14 16:42:01 +0800

    feat(LeetCode): 数组的相对排序

--- src/1122-数组的相对排序/index.js
+++ src/1122-数组的相对排序/index.js
@@ -3,17 +3,24 @@
  * @param {number[]} arr2
  * @return {number[]}
  */
-var relativeSortArray = function (arr1, arr2) {
-  const map = new Map();
-  arr2.forEach((item, index) => {
-    map.set(item, index);
-  });
+// var relativeSortArray = function (arr1, arr2) {
+//   const map = new Map();
+//   arr2.forEach((item, index) => {
+//     map.set(item, index);
+//   });
 
-  return arr1.sort((a, b) => {
-    a = map.has(a) ? map.get(a) : a;
-    b = map.has(b) ? map.get(b) : b;
-    return a - b;
-  });
+//   return arr1.sort((a, b) => {
+//     a = map.has(a) ? map.get(a) : a;
+//     b = map.has(b) ? map.get(b) : b;
+//     return a - b;
+//   });
+// };
+
+var relativeSortArray = function (arr1, arr2) {
+  return arr1.sort(
+    (a, b, i = arr2.indexOf(a), j = arr2.indexOf(b)) =>
+      (i === -1 ? 1000 + a : i) - (j === -1 ? 1000 + b : j)
+  );
 };
 
 console.log(
