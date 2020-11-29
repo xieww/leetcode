9cec56ccef08d1d00a86ed389a3a9298a783e335
xieww <2801416900@qq.com>
2020-11-29 15:29:20 +0800

    feat(LeetCode): 三角形的最大周长

---
+++ src/976-三角形的最大周长/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var largestPerimeter = function (A) {
+  A.sort((a, b) => a - b);
+  for (let i = A.length - 1; i >= 2; i++) {
+    if (A[i - 2] + A[i - 1] > A[i]) {
+      return A[i - 2] + A[i - 1] + A[i];
+    }
+  }
+  return 0;
+};
+
+console.log("should be ", largestPerimeter([2, 1, 2]));
+console.log("should be ", largestPerimeter([1, 2, 1]));
+console.log("should be ", largestPerimeter([3, 2, 3, 4]));
+console.log("should be ", largestPerimeter([3, 6, 2, 3]));
