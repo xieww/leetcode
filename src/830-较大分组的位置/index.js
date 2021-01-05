717323e6ac39df41f0ba41a5551ded5d686d38a6
Chasen.xie <chasen.xie@trustasia.com>
2021-01-05 14:08:44 +0800

    feat(LeetCode): 较大分组的位置

--- src/830-较大分组的位置/index.js
+++ src/830-较大分组的位置/index.js
@@ -17,7 +17,7 @@ var largeGroupPositions = function (s) {
   return result;
 };
 
-// 时间复杂度O(n)
+// 时间复杂度O(n),空间复杂度O(1)
 var largeGroupPositions = function (s) {
   let count = 1;
   let startIndex = 0;
@@ -39,6 +39,33 @@ var largeGroupPositions = function (s) {
   }
   return result;
 };
+
+var largeGroupPositions = function (s) {
+  const result = [];
+  const n = s.length;
+  let num = 1;
+  for (let i = 0; i < n; i++) {
+    if (i === n - 1 || s[i] !== s[i + 1]) {
+      if (num >= 3) {
+        result.push([i - num + 1, i]);
+      }
+      num = 1;
+    } else {
+      num++;
+    }
+  }
+  return result;
+};
+
+function largeGroupPositions(s) {
+  let regex = /(\w)\1{2,}/g;
+  const result = [];
+  s.replace(regex, (val, _, y) => {
+    result.push([y, y + val.length - 1]);
+    return "";
+  });
+  return result;
+}
 
 console.log("[[3,6]]", largeGroupPositions("abbxxxxzzy"));
 console.log("[]", largeGroupPositions("abc"));
