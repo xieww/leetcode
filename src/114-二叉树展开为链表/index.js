893a073351d4b5411e72238614405d688cad92f0
Chasen.xie <chasen.xie@trustasia.com>
2021-01-13 09:46:33 +0800

    feat(LeetCode): 二叉树展开为链表

--- src/114-二叉树展开为链表/index.js
+++ src/114-二叉树展开为链表/index.js
@@ -16,10 +16,16 @@ var flatten = function (root) {
   }
   flatten(root.left);
   flatten(root.right);
+
+  // 后续遍历，左右子树拉平成一条链表
   let left = root.left;
   let right = root.right;
+
+  // 将左子树作为右子树，原左子树置空
   root.left = null;
   root.right = left;
+
+  // 将原来的右子树拼接到当前右子树的末端
   let node = root;
   while (node.right !== null) {
     node = node.right;
