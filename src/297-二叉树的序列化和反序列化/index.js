b3c70d418d46102064940850a7c8231a665e69b4
Chasen.xie <chasen.xie@trustasia.com>
2021-02-02 09:17:52 +0800

    feat(LeetCode): 二叉树的序列化和反序列化

--- src/297-二叉树的序列化和反序列化/index.js
+++ src/297-二叉树的序列化和反序列化/index.js
@@ -22,7 +22,7 @@ class TreeNode {
  * @return {string}
  */
 // var serialize = function (root) {
-//   let str = new String();
+//   let str = "";
 
 //   const helper = (root) => {
 //     if (root === null) {
@@ -63,6 +63,95 @@ class TreeNode {
 //   return helper(result);
 // };
 
+/******************后序遍历*********************/
+
+// var serialize = function (root) {
+//   let str = "";
+
+//   const helper = (root) => {
+//     if (root === null) {
+//       str = str + "#" + ",";
+//       return;
+//     }
+
+//     helper(root.left);
+//     helper(root.right);
+//     str = str + root.val + ",";
+//   };
+//   helper(root);
+//   return str.toString();
+// };
+
+// var deserialize = function (data) {
+//   const result = data.split(",");
+//   const helper = (nodes) => {
+//     if (!nodes || nodes.length === 0) {
+//       return null;
+//     }
+
+//     const last = nodes.pop();
+//     if (last === "#") {
+//       return null;
+//     }
+//     const root = new TreeNode(last);
+//     root.right = helper(nodes);
+//     root.left = helper(nodes);
+//     return root;
+//   };
+//   return helper(result);
+// };
+
+/******************层级遍历*********************/
+
+var serialize = function (root) {
+  if (root === null) {
+    return;
+  }
+  let str = "";
+  const arr = [root];
+  while (arr.length) {
+    const node = arr.shift();
+    if (node === null) {
+      str = str + "#" + ",";
+      continue;
+    }
+    str = str + node.val + ",";
+    arr.push(node.left);
+    arr.push(node.right);
+  }
+  return str;
+};
+
+var deserialize = function (data) {
+  if (data === null) {
+    return null;
+  }
+  const arr = data.split(",");
+  const root = new TreeNode(arr[0]);
+  const queue = [root];
+  for (let i = 1; i < arr.length; ) {
+    const node = queue.shift();
+    const left = arr[i++];
+    if (node) {
+      if (left !== "#") {
+        node.left = new TreeNode(left);
+        queue.push(node.left);
+      } else {
+        node.left = null;
+      }
+
+      const right = arr[i++];
+      if (right !== "#") {
+        node.right = new TreeNode(right);
+        queue.push(node.right);
+      } else {
+        node.right = null;
+      }
+    }
+  }
+  return root;
+};
+
 const tree = {
   val: 1,
   left: {
@@ -79,44 +168,6 @@ const tree = {
     left: null,
     right: null,
   },
-};
-
-/******************后序遍历*********************/
-
-var serialize = function (root) {
-  let str = new String();
-
-  const helper = (root) => {
-    if (root === null) {
-      str = str + "#" + ",";
-      return;
-    }
-
-    helper(root.left);
-    helper(root.right);
-    str = str + root.val + ",";
-  };
-  helper(root);
-  return str.toString();
-};
-
-var deserialize = function (data) {
-  const result = data.split(",");
-  const helper = (nodes) => {
-    if (!nodes || nodes.length === 0) {
-      return null;
-    }
-
-    const last = nodes.pop();
-    if (last === "#") {
-      return null;
-    }
-    const root = new TreeNode(last);
-    root.right = helper(nodes);
-    root.left = helper(nodes);
-    return root;
-  };
-  return helper(result);
 };
 
 console.log(serialize(tree));
