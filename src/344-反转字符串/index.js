e4aa23817bf87ec2a85edafd756dccf11952fd73
Chasen.xie <chasen.xie@trustasia.com>
2021-03-22 10:49:48 +0800

    feat(LeetCode): 反转字符串

--- src/344-反转字符串/index.js
+++ src/344-反转字符串/index.js
@@ -2,8 +2,34 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
+// var reverseString = function (s) {
+//   return s.reverse();
+// };
+
+// var reverseString = function (s) {
+//   if (s === undefined || s.length <= 1) {
+//     return s;
+//   }
+//   const length = s.length;
+//   for (let left = 0, right = length - 1; left < right; left++, right--) {
+//     [s[left], s[right]] = [s[right], s[left]];
+//   }
+//   return s;
+// };
+
 var reverseString = function (s) {
-  return s.reverse();
+  if (s === undefined || s.length <= 1) {
+    return s;
+  }
+  const length = s.length;
+  let left = 0;
+  let right = length - 1;
+  while (left < right) {
+    [s[left], s[right]] = [s[right], s[left]];
+    left++;
+    right--;
+  }
+  return s;
 };
 
 console.log(
