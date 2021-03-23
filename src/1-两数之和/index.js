e92f4a6e441731f05592bb1d5f034a0cc3493b47
Chasen.xie <chasen.xie@trustasia.com>
2021-03-23 09:24:59 +0800

    feat(LeetCode): 两数之和

--- src/1-两数之和/index.js
+++ src/1-两数之和/index.js
@@ -3,14 +3,36 @@
  * @param {number} target
  * @return {number[]}
  */
+// var twoSum = function (nums, target) {
+//   var temp = [];
+//   for (var i = 0; i < nums.length; i++) {
+//     var dif = target - nums[i];
+//     if (temp[dif] !== undefined) {
+//       return [temp[dif], i];
+//     }
+//     temp[nums[i]] = i;
+//   }
+// };
+
+// var twoSum = function (nums, target) {
+//   const map = new Map();
+//   for (let i = 0; i < nums.length; i++) {
+//     const dif = target - nums[i];
+//     if (map.has(dif)) {
+//       return [map.get(dif), i];
+//     }
+//     map.set(nums[i], i);
+//   }
+// };
+
 var twoSum = function (nums, target) {
-  var temp = [];
-  for (var i = 0; i < nums.length; i++) {
-    var dif = target - nums[i];
-    if (temp[dif] !== undefined) {
-      return [temp[dif], i];
+  const obj = {};
+  for (let i = 0; i < nums.length; i++) {
+    const dif = target - nums[i];
+    if (obj[dif] !== undefined) {
+      return [obj[dif], i];
     }
-    temp[nums[i]] = i;
+    obj[nums[i]] = i;
   }
 };
 
