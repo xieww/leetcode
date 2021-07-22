e52742462c790dd2f6efb8543027d5a1b7e10d4e
Chasen.xie <chasen.xie@trustasia.com>
2021-07-22 09:29:02 +0800

    feat(LeetCode): 复制带随机指针的链表

--- src/138-复制带随机指针的链表/index.js
+++ src/138-复制带随机指针的链表/index.js
@@ -12,21 +12,39 @@
  * @param {Node} head
  * @return {Node}
  */
- var copyRandomList = function (head) {
+var copyRandomList = function (head) {
   if (!head) {
-      return null;
+    return null;
   }
   const clones = new Map();
   let n = head;
   while (n) {
-      clones.set(n, new Node(n.val));
-      n = n.next
+    clones.set(n, new Node(n.val));
+    n = n.next;
   }
   n = head;
   while (n) {
-      clones.get(n).next = clones.get(n.next) || null;
-      clones.get(n).random = clones.get(n.random) || null;
-      n = n.next
+    clones.get(n).next = clones.get(n.next) || null;
+    clones.get(n).random = clones.get(n.random) || null;
+    n = n.next;
   }
   return clones.get(head);
 };
+
+function copyRandomList(head) {
+  const map = new Map();
+
+  function copy(node) {
+    if (node == null) return null;
+    if (map.get(node) != null) return map.get(node);
+
+    const n = new Node(node.val);
+    map.set(node, n);
+
+    n.next = copy(node.next);
+    n.random = copy(node.random);
+    return n;
+  }
+
+  return copy(head);
+}
