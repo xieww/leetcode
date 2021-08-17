d4416e725862436721850b319044daad2d8f24c9
Chasen.Xie <chasen.xie@trustasia.com>
2021-08-17 08:25:58 +0800

    feat(LeetCode): 学生出勤记录 I

--- src/551-学生出勤记录 I/index.js
+++ src/551-学生出勤记录 I/index.js
@@ -2,21 +2,22 @@
  * @param {string} s
  * @return {boolean}
  */
-var checkRecord = function (s) {
-  let count = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === s[i + 2] && s[i] === s[i + 1] && s[i] === "L") {
-      return false;
-    }
-    if (s[i] === "A") {
-      count++;
-    }
-  }
-  return count < 2;
-};
+// var checkRecord = function (s) {
+//   let count = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] === s[i + 2] && s[i] === s[i + 1] && s[i] === "L") {
+//       return false;
+//     }
+//     if (s[i] === "A") {
+//       count++;
+//     }
+//   }
+//   return count < 2;
+// };
 
 var checkRecord = function (s) {
-  return !s.match(/.*(A.*A|LLL).*/);
+  // return !s.match(/.*(A.*A|LLL).*/);
+  return !s.match(".*LLL.*|.*A.*A.*");
 };
 
 console.log("should be true", checkRecord("PPALLP"));
