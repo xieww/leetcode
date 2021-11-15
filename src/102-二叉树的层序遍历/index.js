365477d6e0d5bf2cbfebc1a12981fac4a3bc6e11
Chasen.Xie <chasen.xie@trustasia.com>
2021-11-15 08:55:31 +0800

    feat(LeetCode): leetcode

--- src/102-二叉树的层序遍历/index.js
+++ src/102-二叉树的层序遍历/index.js
@@ -31,6 +31,50 @@ var levelOrder = function (root) {
   return levels;
 };
 
+// 变形
+// var levelOrder = function (root) {
+//   if (!root) return [];
+//   const queue = [root];
+//   const levels = [];
+//   while (queue.length !== 0) {
+//     const queueLength = queue.length;
+//     let temp = null;
+//     for (let i = 0; i < queueLength; i++) {
+//       const current = queue.shift();
+//       temp = current.val;
+//       if (current.left) {
+//         queue.push(current.left);
+//       }
+//       if (current.right) {
+//         queue.push(current.right);
+//       }
+//     }
+//     levels.push(temp);
+//   }
+//   return levels;
+// };
+
+// 变形
+// var levelOrder = function (root) {
+//   if (!root) {
+//     return [];
+//   }
+//   const queue = [root];
+//   const result = [];
+//   while (queue.length) {
+//     const node = queue.shift();
+//     result.push(node.val);
+//     if (node.left) {
+//       queue.push(node.left);
+//     }
+//     if (node.right) {
+//       queue.push(node.right);
+//     }
+//   }
+//   return result;
+// };
+
+
 let root = {
   val: 1,
   left: {
