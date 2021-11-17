2fa29db30f7dda7437146b5b26806e7b839867bf
Chasen.Xie <chasen.xie@trustasia.com>
2021-11-17 08:28:44 +0800

    feat(LeetCode): 二叉树的锯齿形层序遍历

---
+++ src/103-二叉树的锯齿形层序遍历/index.js
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {
+  if (!root) {
+    return [];
+  }
+
+  const queue = [root];
+  const result = [];
+  while (queue.length) {
+    const levels = [];
+    const len = queue.length;
+    for (let i = 0; i < len; i++) {
+      const node = queue.shift();
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+      levels.push(node.val);
+    }
+    result.push(result?.length % 2 ? levels.reverse() : levels);
+  }
+  return result;
+};
+
+const root = {
+  val: 3,
+  left: {
+    val: 9,
+    left: null,
+    right: null,
+  },
+  right: {
+    val: 20,
+    left: {
+      val: 15,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 7,
+      left: null,
+      right: null,
+    },
+  },
+};
+
+console.log(zigzagLevelOrder(null));
+console.log(zigzagLevelOrder(root));
